983b60f84b6c512454273a575697de90ea68e2da
zeero <zeero26@gmail.com>
2020-01-21 08:53:02 +0900

    create UI

---
+++ WorkArchitecture/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  WorkArchitecture
+//
+//  Created by zeero on 2020/01/21.
+//  Copyright © 2020 zeero. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
