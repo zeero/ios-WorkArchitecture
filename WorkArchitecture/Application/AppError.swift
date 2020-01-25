c28548cf5e6968df4e43876fae39169fbd3d0aa5
zeero <zeero26@gmail.com>
2020-01-25 23:24:31 +0900

    AppError導入

---
+++ WorkArchitecture/Application/AppError.swift
@@ -0,0 +1,14 @@
+//
+//  AppError.swift
+//  WorkArchitecture
+//
+//  Created by zeero on 2020/01/25.
+//  Copyright © 2020 zeero. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: String, Error {
+    case unknown
+    case apiError
+}
