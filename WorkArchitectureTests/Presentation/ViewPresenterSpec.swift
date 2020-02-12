a32e0519b4115a64d50536459ca6e419dd82fc34
zeero <zeero26@gmail.com>
2020-02-12 09:45:25 +0900

    PresenterSpecをVCMock使わずに動かす

--- WorkArchitectureTests/Presentation/ViewPresenterSpec.swift
+++ WorkArchitectureTests/Presentation/ViewPresenterSpec.swift
@@ -16,29 +16,16 @@ class ViewPresenterSpec: QuickSpec {
 
     override func spec() {
         
-        let disposeBag = DisposeBag()
-        
         describe("button tapped") {
             it("can test") {
                 let router = RouterMock()
                 let presenter = ViewPresenter(router: router)
                 
-                let view = ViewControllerMock()
-                disposeBag.insert([
-                    view.outputPort.fg.bind(to: presenter.inputPort.fg),
-                    view.outputPort.bg.bind(to: presenter.inputPort.bg),
-                    view.outputPort.buttonTap.bind(to: presenter.inputPort.buttonTap)
-                ])
-                view.fg.text = "foo"
-                view.bg.text = "bar"
-                view.button.sendActions(for: .touchUpInside)
+                presenter.inputPort.fg.accept("foo")
+                presenter.inputPort.bg.accept("bar")
+                presenter.inputPort.buttonTap.accept(())
                 
-                waitUntil(timeout: 3.0) { done in
-                    if let message = router._message {
-                        expect(message).to(equal("不正な値です"))
-                        done()
-                    }
-                }
+                expect(router._message).to(equal("不正な値です"))
             }
         }
     }
@@ -61,15 +48,4 @@ class RouterMock: Wireframe {
     func showResult(_ model: ResultViewModel) {
         _model = model
     }
-}
-
-class ViewControllerMock {
-    lazy var outputPort = (
-        fg: fg.rx.text,
-        bg: bg.rx.text,
-        buttonTap: button.rx.tap
-    )
-    let fg = UITextField()
-    let bg = UITextField()
-    let button = UIButton()
 }
